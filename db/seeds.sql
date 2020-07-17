529ec4bf4f092814a351da5868ec908b7b8dbf82
Abel JM SAVOEDA <60677888+savabel@users.noreply.github.com>
2020-07-17 16:49:39 -0400

    First Commit and Publishing

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('bananas', 'from Canada', 5.00, 'fruit_banana.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('pears', 'from Florida', 5.00, 'fruit_pears.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('limes', 'not too sour', 5.00, 'fruits_lemon.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('carrots', 'good for the eyes', 5.00, 'carrot.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('tomatoes', 'sweet tomatoes', 5.00, 'vegetable_brocolli.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('brocolli', 'good for the immune system', 5.00, 'vegetable_brocolli.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('peppers', 'sweet peppers', 5.00, 'vegetable_pepperS.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+INSERT INTO localshop.Products (name, description, price, picture, createdAT, updatedAT)
+VALUES ('celery', 'very few calories', 5.00, 'vegetable_celery.PNG', '2020-05-26 11:30:00', '2020-05-26 11:31:00');
+SELECT * FROM localshop.Products;
